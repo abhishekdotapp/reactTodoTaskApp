61fae64a3e7626dc4b8831b39dd26b831b09b179
Abhishek <103208789+abhishekdotapp@users.noreply.github.com>
2024-11-30 16:21:37 +0530

    Update TodoCard.jsx

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,22 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function TodoCard(props) {
-  const {todo,todoIndex,completeTodo, deleteTodo} = props
-  
+  const { todo, todoIndex, completeTodo, deleteTodo, updateTodo } = props;
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedText, setUpdatedText] = useState(todo.input);
+
   return (
     <div className="card todo-item">
-      <p>{todo.input}</p>
+      {isEditing ? (
+        <input
+          type="text"
+          value={updatedText}
+          onChange={(e) => setUpdatedText(e.target.value)}
+        />
+      ) : (
+        <p>{todo.input}</p>
+      )}
       <div className="todo-buttons">
-        <button disabled={todo.complete} onClick={()=>{
-          completeTodo(todoIndex)
-        }}><h4>Done</h4></button>
-        <button onClick={()=>{
-          deleteTodo(todoIndex)
-        }}><h4>Delete</h4></button>
+        {!isEditing ? (
+          <>
+            <button
+              disabled={todo.complete}
+              onClick={() => completeTodo(todoIndex)}
+            >
+              <h4>Done</h4>
+            </button>
+            <button onClick={() => deleteTodo(todoIndex)}>
+              <h4>Delete</h4>
+            </button>
+            <button onClick={() => setIsEditing(true)}>
+              <h4>Update</h4>
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              onClick={() => {
+                updateTodo(todoIndex, updatedText);
+                setIsEditing(false);
+              }}
+            >
+              <h4>Save</h4>
+            </button>
+            <button onClick={() => setIsEditing(false)}>
+              <h4>Cancel</h4>
+            </button>
+          </>
+        )}
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default TodoCard
+export default TodoCard;
+        
